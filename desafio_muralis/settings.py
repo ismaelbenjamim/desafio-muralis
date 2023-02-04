6dba671afb731792961bca653f10a0875d5f6f5d
Ismael Benjamim <60023762+ismaelbenjamim@users.noreply.github.com>
2023-02-04 00:45:21 -0300

    Alteração no settings

--- desafio_muralis/settings.py
+++ desafio_muralis/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4$c$bif389ty74*d-d-++4a9upy$0knu18ivnr=muk)8+*(vog'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
